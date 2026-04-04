0ea3ab4d3a614091500c23e716a2f93a3b97d9bd
Sarana Adhikari <sarana.adhikari1@gmail.com>
2026-04-04 16:51:03 -0400

    feat: Add GitHub Actions CI/CD pipeline

--- infra/main.tf
+++ infra/main.tf
@@ -47,8 +47,13 @@ resource "aws_lambda_function" "pantry_backend" {
 resource "aws_apigatewayv2_api" "pantry_api" {
   name          = "pantry-to-plate-gateway"
   protocol_type = "HTTP"
-}
 
+  cors_configuration {
+    allow_origins = ["*"] # In production, I'd limit this to my CloudFront URL
+    allow_methods = ["POST", "OPTIONS"]
+    allow_headers = ["content-type"]
+  }
+}
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id             = aws_apigatewayv2_api.pantry_api.id
   integration_type   = "AWS_PROXY"
@@ -88,4 +93,92 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
 
   # Automatically delete logs after 14 days to prevent unnecessary storage costs
   retention_in_days = 0
+}
+
+# 6. S3 Bucket for Static Website
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "pantry-to-plate-frontend-${random_id.id.hex}" # Needs to be unique
+}
+
+resource "random_id" "id" {
+  byte_length = 4
+}
+
+# 7. CloudFront Origin Access Control (OAC) 
+# This ensures people can't bypass CloudFront to access your S3 bucket directly
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "s3_oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# 8. CloudFront Distribution
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
+    origin_id                = "S3Origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3Origin"
+
+    forwarded_values {
+      query_string = false
+      cookies { forward = "none" }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction { restriction_type = "none" }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# 9. S3 Bucket Policy to allow CloudFront to read the files
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.frontend_bucket.arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.s3_distribution.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
+
+# Output the Website URL
+output "website_url" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.frontend_bucket.bucket
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
 }
